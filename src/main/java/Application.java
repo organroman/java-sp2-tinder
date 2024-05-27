0cb3cdd961a7812272f1bdfc596e195df4294ec6
Roman Organ <r.organ@dan-it.com.ua>
2024-05-27 19:57:36 +0300

    added messages servlet with hardcoded html display, added staticContentServlet for handling css

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,19 +1,23 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.HelloServlet;
-import servlets.TemplateEngine;
-import servlets.UserServlet;
+import servlets.*;
+
+import javax.servlet.http.HttpServlet;
 
 public class Application {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
+
         TemplateEngine te = new TemplateEngine("templates");
         ServletContextHandler handler = new ServletContextHandler();
         handler.addServlet(new ServletHolder(new HelloServlet()), "/");
 
         server.setHandler(handler);
         handler.addServlet(new ServletHolder(new UserServlet(te)), "/users");
+        handler.addServlet(new ServletHolder(new ChatServlet(te)), "/messages");
+
+        handler.addServlet(new ServletHolder(new StaticContentServlet("css")), "/css/*");
 
         server.setHandler(handler);
         server.start();
--- src/main/java/servlets/ResourceOps.java
+++ src/main/java/servlets/ResourceOps.java
@@ -6,12 +6,14 @@ public class ResourceOps {
 
     public static String dirUnsafe(String prefix) {
         try {
-            String s = ResourceOps.class
-                    .getClassLoader()
-                    .getResource(prefix)
-                    .toURI()
-                    .getPath();    //    "/C:/*"
-            return s.substring(s.lastIndexOf(':') - 1); // delete symbols '/' before drive C: if exist
+//            String s =
+            return
+                    ResourceOps.class
+                            .getClassLoader()
+                            .getResource(prefix)
+                            .toURI()
+                            .getPath();    //    "/C:/*"
+//            return s.substring(s.lastIndexOf(':') - 1); // delete symbols '/' before drive C: if exist
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
