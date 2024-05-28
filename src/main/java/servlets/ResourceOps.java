c31744e43985e105537eb9c6a990bbe10f4a53d1
Roman Organ <r.organ@dan-it.com.ua>
2024-05-28 11:44:07 +0300

    added condition for splitting C from the path in resourcses

--- src/main/java/servlets/ResourceOps.java
+++ src/main/java/servlets/ResourceOps.java
@@ -1,19 +1,22 @@
 package servlets;
 
 import java.net.URISyntaxException;
+import java.nio.file.Paths;
 
 public class ResourceOps {
 
     public static String dirUnsafe(String prefix) {
         try {
-//            String s =
-            return
+            String path =
                     ResourceOps.class
                             .getClassLoader()
                             .getResource(prefix)
                             .toURI()
-                            .getPath();    //    "/C:/*"
-//            return s.substring(s.lastIndexOf(':') - 1); // delete symbols '/' before drive C: if exist
+                            .getPath();
+            if (path.startsWith("/C:")) {
+                path = path.substring(3);
+            }
+            return Paths.get(path).toString();  // delete symbols '/' before drive C: if exist
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
