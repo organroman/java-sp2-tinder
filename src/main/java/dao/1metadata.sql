e258a49783ae8f109d25dd10c53811b5f05ce912
Roman Organ <r.organ@dan-it.com.ua>
2024-05-31 13:53:04 +0300

    upd metadata

--- src/main/java/dao/1metadata.sql
+++ src/main/java/dao/1metadata.sql
@@ -1,5 +1,5 @@
-create
-database user_step_base;
+--create
+--database user_step_base; todo: delete
 
 
 create table users
@@ -7,11 +7,33 @@ create table users
     user_id   serial
         constraint student_pk primary key,
     name text,
-    logo text
+    logo text,
+    password text
 );
 
-insert into users (name,logo) values ('MXP','https://stud-point.com/wp-content/uploads/2024/01/logo_MKHP.png');
-insert into users (name,logo) values ('Farro','https://farro.org.ua/templates/Faro_lang/images/faro-logo.png');
-insert into users (name,logo) values ('GLB','https://corp.globino.ua/wp-content/uploads/2020/05/logo.png');
-insert into users (name,logo) values ('John Doe','https://robohash.org/johndoe.png');
-insert into users (name,logo) values ('Jane Doe','https://robohash.org/janesmith.png');
+insert into users (name,logo) values ('MXP','https://stud-point.com/wp-content/uploads/2024/01/logo_MKHP.png', '123456');
+insert into users (name,logo) values ('Farro','https://farro.org.ua/templates/Faro_lang/images/faro-logo.png', '123456');
+insert into users (name,logo) values ('GLB','https://corp.globino.ua/wp-content/uploads/2020/05/logo.png', '123456');
+insert into users (name,logo) values ('John Doe','https://robohash.org/johndoe.png', '123456');
+insert into users (name,logo) values ('Jane Doe','https://robohash.org/janesmith.png' '123456');
+
+create table likes
+(
+    id     serial
+        constraint likes_pk
+            primary key,
+    who    integer,
+    whom   integer,
+    action text
+);
+
+create table messages
+(
+    id     serial
+        constraint messages_pk
+            primary key,
+    chat_owner  integer,
+    receiver   integer,
+    content text,
+    date timestamp
+);
