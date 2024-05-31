010e747d406131cd213599d842f272596582aa74
Roman Organ <r.organ@dan-it.com.ua>
2024-05-31 13:09:47 +0300

    docker added

--- src/main/java/dao/1metadata.sql
+++ src/main/java/dao/1metadata.sql
@@ -4,7 +4,7 @@ database user_step_base;
 
 create table users
 (
-    id   serial
+    user_id   serial
         constraint student_pk primary key,
     name text,
     logo text
@@ -14,4 +14,4 @@ insert into users (name,logo) values ('MXP','https://stud-point.com/wp-content/u
 insert into users (name,logo) values ('Farro','https://farro.org.ua/templates/Faro_lang/images/faro-logo.png');
 insert into users (name,logo) values ('GLB','https://corp.globino.ua/wp-content/uploads/2020/05/logo.png');
 insert into users (name,logo) values ('John Doe','https://robohash.org/johndoe.png');
-insert into users (name,logo) values ('https://robohash.org/janesmith.png');
+insert into users (name,logo) values ('Jane Doe','https://robohash.org/janesmith.png');
