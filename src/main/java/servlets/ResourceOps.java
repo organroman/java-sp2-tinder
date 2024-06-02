84eff094d764a29f0e0f63ec963ee9c39c23276c
Roman Organ <r.organ@dan-it.com.ua>
2024-06-02 22:54:09 +0300

    remove unneccessary code

--- src/main/java/servlets/ResourceOps.java
+++ src/main/java/servlets/ResourceOps.java
@@ -1,8 +1,7 @@
 package servlets;
 
-import java.net.URISyntaxException;
+
 import java.net.URL;
-import java.nio.file.Paths;
 
 public class ResourceOps {
     public static URL getResource(String prefix) {
@@ -12,21 +11,4 @@ public class ResourceOps {
         }
         return resourceUrl;
     }
-//TODO: REMOVE
-//   public static String dirUnsafe(String prefix) {
-//        try {
-//            String path =
-//                    ResourceOps.class
-//                            .getClassLoader()
-//                            .getResource(prefix)
-//                            .toURI()
-//                            .getPath();
-//            if (path.startsWith("/C:")) {
-//                path = path.substring(3);
-//            }
-//            return Paths.get(path).toString();  // delete symbols '/' before drive C: if exist
-//        } catch (URISyntaxException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
--- src/main/java/servlets/StaticContentServlet.java
+++ src/main/java/servlets/StaticContentServlet.java
@@ -6,58 +6,24 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 public class StaticContentServlet extends HttpServlet {
 
     private final String prefix;
 
     public StaticContentServlet(String root) {
-//        TODO: REMOVE
-//        this.prefix = ResourceOps.dirUnsafe(root);
         this.prefix = root;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        // 1
         String fileName = req.getPathInfo();
         String fullName = prefix + fileName;
-//TODO: REMOVE
-//        System.out.println(fileName);
-//        System.out.println(fullName);
 
-//        if (!new File(fullName).exists()) {
-//            // 3
-//            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//        } else try (ServletOutputStream os = resp.getOutputStream()) {
-//            // 2
-//            Path path = Paths.get(fullName);
-//            // 3
-//            Files.copy(path, os);
-//        }
-//        InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(fullName);
-//        if (resourceStream == null) {
-//            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//        } else {
-//            try (ServletOutputStream os = resp.getOutputStream()) {
-//                byte[] buffer = new byte[1024];
-//                int bytesRead;
-//                while ((bytesRead = resourceStream.read(buffer)) != -1) {
-//                    os.write(buffer, 0, bytesRead);
-//                }
-//            } finally {
-//                resourceStream.close();
-//            }
-//        }
-//    }
-//}
         InputStream resourceStream = getClass().getClassLoader().getResourceAsStream(fullName);
         if (resourceStream == null) {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
