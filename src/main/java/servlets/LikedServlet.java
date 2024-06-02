3dca6fb5be9acdb325ee62ec4702d30245c77b32
egormyronchuk <egormyronchuk@gmail.com>
2024-06-02 15:53:53 +0300

    finish with liked servlet

--- src/main/java/servlets/LikedServlet.java
+++ src/main/java/servlets/LikedServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
+import dao.LikesDAO;
 import models.User;
+import service.AuthService;
+import service.LikeService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.sql.SQLException;
+import java.util.*;
 
 public class LikedServlet extends HttpServlet {
     private final TemplateEngine te;
@@ -21,23 +22,35 @@ public class LikedServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LikeService likesDAO = new LikeService();
         List<User> likedUsers = new ArrayList<>();
-        likedUsers.add(new User(1,"John Doe", "https://robohash.org/johndoe.png"));
-        likedUsers.add(new User(2,"Jane Smith", "https://robohash.org/janesmith.png"));
-        likedUsers.add(new User(3,"J4ane Smith", "https://robohash.org/janesmith.png"));
-        likedUsers.add(new User(4,"Ja1ne Smith", "https://robohash.org/janesmith.png"));
-        likedUsers.add(new User(5,"Jan2e Smith", "https://robohash.org/janesmith.png"));
-        likedUsers.add(new User(6,"Ja3ne Smith", "https://robohash.org/janesmith.png"));
-        // Добавьте больше пользователей по желанию
 
-        //System.out.println(AuthService.getUserIdByUUID(Auth.getCookieValueUnsafe(req)));
-       // System.out.println(AuthService.getUserByUUID(Auth.getCookieValueUnsafe(req)));
+        Optional<Integer> isLoggedInUserId = AuthService.getUserIdByUUID(Auth.getCookieValueUnsafe(req));
+        int loggedInUserId = isLoggedInUserId.orElseThrow(() -> new ServletException("User is not logged in"));
+
+        try {
+        likedUsers = likesDAO.selectByLike(loggedInUserId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
 
         Map<String, Object> data = new HashMap<>();
         data.put("users", likedUsers);
 
         try (PrintWriter w = resp.getWriter()) {
             te.render("liked-list.ftl", data, w);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userIdParam = req.getParameter("userId");
+        if (userIdParam != null) {
+            int userId = Integer.parseInt(userIdParam);
+            resp.sendRedirect("/messages/" + userId);
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing userId parameter");
         }
     }
 }
